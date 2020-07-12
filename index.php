bee621ca006586ba7df30eca75856b7e80fe6bc9
三浦友嗣 <>
2020-07-12 15:20:20 +0900

    提出

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <head>
   <meta charset="UTF-8" />
   <title>tech boost</title>
+  <!-- ここから追加 -->
+  <style>
+  h1 {
+    color: red;
+  }
+  </style>
+<!-- ここまで追加 -->
 </head>
   <body>
     <h1>Hello world!!!</h1>
